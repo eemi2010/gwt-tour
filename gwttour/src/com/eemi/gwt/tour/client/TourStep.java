687e79679c0545ae895b2d577cbd246682f1c604
eemi2010 <jazzmatadazz@gmail.com>
2013-06-26 23:40:10 +0200

    Fixed typo in setCTALabel method

--- gwttour/src/com/eemi/gwt/tour/client/TourStep.java
+++ gwttour/src/com/eemi/gwt/tour/client/TourStep.java
@@ -1,18 +1,18 @@
 /************************************************************************
-  TourStep.java is part of Gwt-Tour 0.1  Copyright 2013 Alain Ekambi
-
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-**************************************************************************/
+ * TourStep.java is part of Gwt-Tour 0.1 Copyright 2013 Alain Ekambi
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ **************************************************************************/
 package com.eemi.gwt.tour.client;
 
 import com.eemi.gwt.tour.client.jso.Function;
@@ -233,7 +233,7 @@ public class TourStep {
      * @param label
      */
     public void setCTALabel(String label) {
-        JsoHelper.setAttribute(peer, "ctalLabel", label);
+        JsoHelper.setAttribute(peer, "ctaLabel", label);
     }
 
     /**
